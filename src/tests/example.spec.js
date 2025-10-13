350361c495c6a226cfbe8c9bbbdfb2e78504be21
lsevcikova <lucie.sevcikova108@gmail.com>
2025-10-13 19:10:57 +0200

    initial commint

--- src/tests/example.spec.js
+++ src/tests/example.spec.js
@@ -2,5 +2,7 @@ import { test } from "@playwright/test";
 
 test("should open login page", async ({ page }) => {
     await page.goto("/prihlaseni");
+    await page.locator("input#email").screenshot({ path: "css_id_email.png" });
+    await page.locator(".btn-primary").screenshot({ path: "submit_btn.png" });
     console.log(await page.title());
 });
